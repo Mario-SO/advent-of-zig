b8bd7a1e2ecda504eefb3f559a7dc9ff65a240b4
Mario S.O <gh_mario.proclaim@slmail.me>
2024-04-05 16:04:54 -0400

    Finished part a)

--- src/aoc2016/day1a.zig
+++ src/aoc2016/day1a.zig
@@ -1,10 +1,53 @@
 const input = @embedFile("./i1.txt");
 const std = @import("std");
 
-pub fn main() !void {
-    var it = std.mem.tokenize(u8, input, ",");
+//          3
+//       ┌─────┐
+//       │North│
+//       └─────┘
+//  ┌────┐ .─. ┌────┐
+// 2│West│(   )│East│0
+//  └────┘ `─' └────┘
+//       ┌─────┐
+//       │South│
+//       └─────┘
+//          1
 
-    while (it.next()) |instruction| {
-        std.debug.print("{s}\n", .{instruction});
+const Direction = enum(u32) {
+    North = 3, // Up
+    South = 1, // Down
+    East = 0, // Right
+    West = 2, // Left
+};
+
+const Position = struct {
+    x: i32,
+    y: i32,
+};
+
+pub fn main() !void {
+    var split_iter = std.mem.split(u8, input, ", ");
+    var position: Position = .{ .x = 0, .y = 0 };
+    var dir: Direction = Direction.North;
+
+    while (split_iter.next()) |instr| {
+        const turn = instr[0];
+        const dist = try std.fmt.parseInt(i32, instr[1..], 10);
+
+        switch (turn) {
+            'L' => dir = @as(Direction, @enumFromInt((@intFromEnum(dir) + 3) % 4)),
+            'R' => dir = @as(Direction, @enumFromInt((@intFromEnum(dir) + 1) % 4)),
+            else => return error.InvalidTurn,
+        }
+
+        switch (dir) {
+            .North => position.y += dist,
+            .South => position.y -= dist,
+            .East => position.x += dist,
+            .West => position.x -= dist,
+        }
     }
+
+    const result = @as(u32, std.math.absCast(position.x) + std.math.absCast(position.y));
+    std.debug.print("{}", .{result});
 }
